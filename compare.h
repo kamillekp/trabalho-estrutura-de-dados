22ab9bf145c9fb2829bde11d51d4da258ebf9ef8
Kamille Konarzewski <kamille.kpimentel@gmail.com>
2023-08-06 23:28:30 -0300

    Gerando dados pseudoaleatorios

--- compare.h
+++ compare.h
@@ -1,7 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// criar structs
-// prototipo das funcoes
+// CRIANDO STRUCTS
+typedef struct avlNode AVLNode;
+struct avlNode {
+    int key;
+    int height;
+    AVLNode *leftKid;
+    AVLNode *rightKid;
+};
+
+typedef struct dllNode DLLNode;
+struct SLLNode {
+    int key;
+    DLLNode *left;
+    DLLNode *right;
+};
+
+// PROTÓTIPOS PARA A ÁRVORE AVL
+
+// PROTÓTIPOS PARA A LISTA DUPLAMENTE ENCADEADA
+void insertDLL(struct DLinkedList **head, int value);
+
 
 
--- main.c
+++ main.c
@@ -1,7 +1,45 @@
+#include <stdlib.h>
 #include <stdio.h>
-#include "compare.h"
+#include <time.h>
+
+void geradados(int n1, int *vetor);
 
 int main(){
 
+    int n1 = 5000, n2 = 10000, n3 = 100000;
+    
+    int dados1 [n1];
+    int dados2 [n2];
+    int dados3 [n3];
+        
+    //PARA O CONJUNTO DE DADOS 1
+    geraDados(n1, dados1);
+    //PARA O CONJUNTO DE DADOS 2
+    geraDados(n2, dados2);
+    //PARA O CONJUNTO DE DADOS 3
+    geraDados(n3, dados3); 
+
     return 0;
+}
+
+void geraDados (int num, int *vetor){
+    srand(time(NULL));
+
+    //GERA 90% DOS DADOS DE FORMA PSEUDOALEATÓRIA
+    for(int i = 0; i < (0.9 * num); i ++){
+        vetor[i] = (rand() % num)+1;
+    }
+
+    //GERA 10% DOS DADOS QUE CERTAMENTE SÃO REPETIDOS
+    for(int i = (0.9 * num); i < num; i ++){
+        //PEGA UM NÚMERO JÁ EXISTENTE NO ARRAY
+        int indice = rand()%(i - 1);
+        int numero = vetor[indice];
+
+        //COLOCA O NUMERO EM UM ÍNDICE QUALQUER DO ARRAY PARA QUE OS REPETIDOS NÃO FIQUEM SÓ NO FINAL
+        indice = rand()%(i - 1);
+        int aux = vetor[indice];
+        vetor[indice] = numero;
+        vetor[i] = aux;
+    }
 }
