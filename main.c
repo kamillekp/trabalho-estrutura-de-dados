7a5cf6c379edb5f19098c405535b948d63ec1950
Kamille Konarzewski <kamille.kpimentel@gmail.com>
2023-08-07 23:31:29 -0300

    tempo de execução para cada estrutura

--- main.c
+++ main.c
@@ -1,12 +1,18 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
+#include <locale.h>
+
+#include "compare.h"
 
 void generateData(int n1, int *vetor);
+void formatTime(long int difference, long int timeDLL, long int timeAVL);
 
 int main(){
+    setlocale(LC_ALL, "Portuguese");
 
     int n1 = 5000, n2 = 10000, n3 = 100000;
+    time_t timeDifference, timeDLL, timeAVL;
     
     int dados1 [n1];
     int dados2 [n2];
@@ -19,7 +25,7 @@ int main(){
     //PARA O CONJUNTO DE DADOS 3
     generateData(n3, dados3); 
 
-
+    time_t startTime = time(NULL);
     // CRIA DLL 
 
     // INSERE NA DLL 
@@ -33,7 +39,11 @@ int main(){
     // 10 MAIS REPETIDO NA DLL 
 
     // 50 MAIS REPETIDO NA DLL 
+    time_t endTime = time(NULL);
 
+    timeDLL = abs(endTime - startTime);
+
+    time_t startTime = time(NULL);
     // CRIA AVL
     
     // INSERE NA AVL
@@ -47,8 +57,13 @@ int main(){
     // 10 MAIS REPETIDO NA AVL 
 
     // 50 MAIS REPETIDO NA AVL
+    time_t endtime = time(NULL);
+    
+    timeAVL = abs(endtime - startTime);
 
-    // VERIFICAR TEMPO DE CADA USANDO TIME.H
+    timeDifference = abs(timeDLL - timeAVL);
+    formatTime(timeDifference, timeDLL, timeAVL);
+
 
     // VERIFICAR DIFERENÇA DO NÚMERO DE COMPARAÇÕES DE CADA (if/while/for...)
 
@@ -77,12 +92,27 @@ void generateData (int num, int *vetor){
     }
 }
 
-int formatTime(int tempo){
-    // FORMATA O TEMPO PARA SER EXIBIDO EM MINUTOS/SEGUNDOS (veremos)
-}
+void formatTime(long int difference, long int timeDLL, long int timeAVL){
+    int flag = 0;
 
-void compareTime(int tempo1, int tempo2){
-    // COMPARA OS TEMPOS DA DLL E DA AVL
+    if(timeDLL > timeAVL)
+        flag = 1;
+
+    if(difference >= 60)
+        if(flag == 1)
+            printf("A DLL demorou %ld minutos a mais que a AVL.\n", difference / 60);
+        else
+            printf("A AVL demorou %ld minutos a mais que a DLL.\n", difference / 60);
+    else if(difference >= 3600)
+        if(flag == 1)
+            printf("A DLL demorou %ld horas a mais que a AVL.\n", difference / 3600);
+        else
+            printf("A AVL demorou %ld horas a mais que a DLL.\n", difference / 3600);
+    else
+        if(flag == 1)
+            printf("A DLL demorou %ld segundos a mais que a AVL.\n", difference);
+        else
+            printf("A AVL demorou %ld segundos a mais que a DLL.\n", difference);
 }
 
 void compareComparisons(int compara1, int compara2){
